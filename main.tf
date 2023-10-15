fd9b549cf0846f2664055166de280fab3671821e
ciaran mccamphill <ciaran.mccamphill@yahoo.com>
2023-10-15 17:50:48 +0100

    Added drone file and terraform

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"  # Change this to your desired region
+}
+
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"  # Change this to your desired availability zone
+  depends_on = [aws_vpc.example_vpc]
+}
