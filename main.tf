0170a32b683b4c2d66cca6d97b713cf11224c23b
ciaran mccamphill <ciaran.mccamphill@yahoo.com>
2023-10-16 23:53:40 +0100

    updated the main.tf with creds

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "us-east-1"  # Change this to your desired region
+  access_key = "AKIAWJVU4T7CC27ZWKUW"
+  secret_key = "30JkjZiUtpiLhjEnUdNWJQJnUtEqvlu4yp+n7mqX"
 }
 
 resource "aws_vpc" "example_vpc" {
